c52c51207118bebd73c02b1d80966578b167b581
Travi <Travi@TRAVIS>
2016-11-07 20:01:37 -0500

    Add project files.

---
+++ KubotaECM/KubotaECM.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		KubotaECM.ino
+ Created:	11/7/2016 8:01:35 PM
+ Author:	Travi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
